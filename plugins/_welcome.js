9da137aa346a25d53ed5332a036354f9414326c6
fedelanyt130 <federicoyt150@gmail.com>
2025-06-12 17:50:30 -0300

    Update _welcome.js

--- plugins/_welcome.js
+++ plugins/_welcome.js
@@ -36,7 +36,7 @@ function obtenerPais(numero) {
 
 export async function before(m, { conn, participants, groupMetadata }) {
   if (!m.messageStubType || !m.isGroup) return;
-  if (m.chat === "120363402481697721@g.us") return;
+  if (m.chat === "120363420017244077@newsletter") return;
 
   let who = m.messageStubParameters[0];
   let taguser = `@${who.split("@")[0]}`;
